abcc95b71c38e5197f2ecf922061cef9c77a31f4
Mikhail Shirkov <shirkevich@gmail.com>
2015-09-02 15:52:42 +0300

    Skip web configure, instead prepare all configs for it

--- templates/createdb.sh
+++ templates/createdb.sh
@@ -1,3 +1,6 @@
 {% for database in database_list %}
-mysql -u {{ database.user }} -h {{ database.host }} -p{{ database.password }} {{ database.name }}< {{ database.dump }}
+mysql -u {{ database.user }} -h {{ database.host }} -p{{ database.password }} {{ database.name }} < {{ database.dump }}
+{% endfor %}
+{% for user in icinga2.web.users %}
+mysql -u {{ database_list[0].user }} -h {{ database_list[0].host }} -p{{ database_list[0].password }} {{ database_list[0].name }} -e 'INSERT INTO icingaweb_user (name, active, password_hash) VALUES ("{{ user.name }}", 1, "{{ user.pass_hash }}");'
 {% endfor %}
