bde6d71101c12f0d7a8410652057eea7bb1b8ab5
Alex Chistyakov <alexclear@gmail.com>
2015-09-30 06:42:36 +0300

    Check if the DB is initialized already and don't initialize it if it is

--- templates/createdb.sh
+++ templates/createdb.sh
@@ -1,5 +1,17 @@
-mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icinga2-ido-mysql/schema/mysql.sql
-mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icingaweb2/etc/schema/mysql.schema.sql
+mysqlshow -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} | grep icinga_acknowledgements
+if [ $? != 0 ]; then
+  date
+  echo "Loading /usr/share/icinga2-ido-mysql/schema/mysql.sql"
+  mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+  echo "...loaded"
+fi
+mysqlshow -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} | grep icingaweb_group
+if [ $? != 0 ]; then
+  date
+  echo "Loading /usr/share/icingaweb2/etc/schema/mysql.schema.sql"
+  mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icingaweb2/etc/schema/mysql.schema.sql
+  echo "....loaded"
+fi
 {% for user in icinga2.web.users %}
 mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} -e 'INSERT INTO icingaweb_user (name, active, password_hash) VALUES ("{{ user.name }}", 1, "{{ user.pass_hash }}");'
 {% endfor %}
