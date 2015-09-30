c62c2cc23a953c865429f11d70f8bd33b72379bd
Alex Chistyakov <alexclear@gmail.com>
2015-09-30 05:49:39 +0300

    Let's name DBs and dumps explicitly since we always know what to load

--- templates/createdb.sh
+++ templates/createdb.sh
@@ -1,6 +1,5 @@
-{% for database in database_list %}
-mysql -u {{ database.user }} -h {{ database.host }} -p{{ database.password }} {{ database.name }} < {{ database.dump }}
-{% endfor %}
+mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} < /usr/share/icingaweb2/etc/schema/mysql.schema.sql
 {% for user in icinga2.web.users %}
-mysql -u {{ database_list[0].user }} -h {{ database_list[0].host }} -p{{ database_list[0].password }} {{ database_list[0].name }} -e 'INSERT INTO icingaweb_user (name, active, password_hash) VALUES ("{{ user.name }}", 1, "{{ user.pass_hash }}");'
+mysql -u {{ icinga2.ido.user }} -h {{ icinga2.ido.dbserver }} -p{{ icinga2.ido.pass }} {{ icinga2.ido.dbname }} -e 'INSERT INTO icingaweb_user (name, active, password_hash) VALUES ("{{ user.name }}", 1, "{{ user.pass_hash }}");'
 {% endfor %}
